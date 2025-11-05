867ad0f81ad9ede717c5465341672ef7833f313a
ZaneWallace20 <138349317+ZaneWallace20@users.noreply.github.com>
2025-11-04 23:24:31 -0600

    check for previously used words

--- wordlist.py
+++ wordlist.py
@@ -1,6 +1,19 @@
+import os
 class Loader:
-    def __init__(self, filepath = 'words.txt'):
+    def __init__(self, filepath = 'words.txt', tested_filepath = None):
         self.filepath = filepath
+        self.tested_filepath = tested_filepath
+
+    def _grab_previously_tested(self):
+        
+        tested_words = set()
+        if self.tested_filepath and os.path.exists(self.tested_filepath):
+            with open(self.tested_filepath, 'r', encoding='utf-8') as f:
+                for line in f:
+                    word = line.strip()
+                    if word:
+                        tested_words.add(word)
+        return tested_words 
 
     def _format_word(self, word):
         word = word.strip()
@@ -10,9 +23,14 @@ class Loader:
         return list(set([word, word.lower(), word.upper()]))
 
     def load_words(self):
+        tested_words = self._grab_previously_tested()
+        wordlist_set = set()
         with open(self.filepath, 'r') as file:
-            
-            words = list(set([formattedWord for line in file if line.strip() for formattedWord in self._format_word(line)]))
+            for line in file:
+                word = line.strip()
+                if word and word not in tested_words:
+                    wordlist_set.add(word)
+        words = [formattedWord for word in wordlist_set for formattedWord in self._format_word(word)]
         
         print(f"Loaded {len(words)} unique words from {self.filepath}")
         return words
